04bac572a0a6e2ef8b60c055d952c474411d3b23
Artur Henrique <arturhenriquedelimatutu@gmail.com>
2025-09-30 22:24:00 -0300

    Authenticação de Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, redirect
+<<<<<<< HEAD
 # Importe a função login do Django e renomeie-a para evitar conflito
 from django.contrib.auth import authenticate, login as django_login
 from django.http.response import HttpResponse
 from .models import User
+=======
+from django.contrib.auth import authenticate, login as django_login
+from django.http.response import HttpResponse
+from django.contrib.auth import login as django_login
+from accounts.backends import EmailOrNomeBackend
+from .models import User
+from .backends import EmailOrNomeBackend
+from django.db.models import Q
+>>>>>>> a7a9e1c (Authenticação de Login)
 from django.urls import reverse
 
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'accounts/cadastro.html')
     else:
+<<<<<<< HEAD
         # Seu código de cadastro
         # ...
+=======
+>>>>>>> a7a9e1c (Authenticação de Login)
         nome = request.POST.get('nome')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
@@ -25,11 +38,17 @@ def cadastro(request):
         
         return render(request, 'accounts/login.html')
     
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> a7a9e1c (Authenticação de Login)
 def login(request):
     if request.method == "POST":
         identificador = request.POST.get('identificador')
         senha = request.POST.get('senha')
 
+<<<<<<< HEAD
         usuario = authenticate(request, username=identificador, password=senha)
         
         if usuario is not None:
@@ -39,3 +58,20 @@ def login(request):
             return HttpResponse("Usuário ou senha inválidos.")
     else:
         return render(request, 'accounts/login.html')
+=======
+        try:
+            user = User.objects.get(Q(nome=identificador) | Q(email=identificador))
+        except User.DoesNotExist:
+            return render(request, 'accounts/login.html', {'error': 'Nome ou e-mail inválido'})
+
+        if user.check_password(senha):
+            user.backend = 'accounts.backends.EmailOrNomeBackend' 
+            django_login(request, user)
+            return redirect(reverse('finance:finance_list'))
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Senha incorreta'})
+
+    return render(request, 'accounts/login.html')
+
+    
+>>>>>>> a7a9e1c (Authenticação de Login)
